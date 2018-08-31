2cd2e85473425076de0cd5ae6bf1689033e7cb14
David Engelstad <davidengelstad@gmail.com>
2018-08-31 12:32:25 -0700

    Exercise 1

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client'),
+  output: {
+    path: path.resolve(__dirname, './static'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js[x]?/,
+        exclude: /node_modules/,
+        options: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
